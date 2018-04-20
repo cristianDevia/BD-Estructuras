be22510e44a352ad95c80e628b30a03ed33f67b6
cristianDevia <c.devia8@gmail.com>
2018-04-19 21:23:24 -0500

    Clase artista
    Un artista basico *PENDIENTE, ID (?)*

--- src/Tablas/Artista.java
+++ src/Tablas/Artista.java
@@ -1,5 +1,52 @@
 package Tablas;
 
 public class Artista {
-
+	
+	private String nombreArtista;
+	
+	private boolean tipoDeArtista;
+	
+	private String pais;
+	
+	
+	public Artista(String pNombreArtista, boolean pTipoDeArtista, String pPais)
+	{
+		
+		nombreArtista = pNombreArtista;
+		tipoDeArtista = pTipoDeArtista;
+		pais = pPais;
+		
+	}
+	
+	public String darNombreArtista()
+	{
+		return nombreArtista;
+	}
+	
+	public boolean darTipoDeArtista()
+	{
+		return tipoDeArtista;
+	}
+	
+	public String darPais()
+	{
+		return pais;
+	}
+	
+	public void cambiarNombreArtista( String pNombreArtista )
+	{
+		nombreArtista = pNombreArtista;
+	}
+	
+	public void cambiarTipoDeArtista( boolean pTipoArtista)
+	{
+		tipoDeArtista = pTipoArtista;
+	}
+	
+	public void cambiarPais( String pPais )
+	{
+		pais = pPais;
+	}
+	
+	
 }
