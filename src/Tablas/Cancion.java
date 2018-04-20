73880f82f7620917b1b43e33a79f27d5fd8d413a
cristianDevia <c.devia8@gmail.com>
2018-04-19 21:28:47 -0500

    Cancion basica
    *PENDIENTE, ID (?)*

--- src/Tablas/Cancion.java
+++ src/Tablas/Cancion.java
@@ -2,4 +2,46 @@ package Tablas;
 
 public class Cancion {
 
+	private String nombreCancion;
+	
+	private int duracion;
+	
+	private String genero;
+	
+	public Cancion (String pNombreCancion, int pDuracion, String pGenero)
+	{
+		nombreCancion = pNombreCancion;
+		duracion = pDuracion;
+		genero = pGenero;
+	}
+	
+	public String darNombreCancion()
+	{
+		return nombreCancion;
+	}
+	
+	public int darDuracion()
+	{
+		return duracion;
+	}
+	
+	public String darGenero()
+	{
+		return genero;
+	}
+	
+	public void cambiarNombreCancion( String pNombreCancion )
+	{
+		nombreCancion = pNombreCancion;
+	}
+	
+	public void cambiarDuracion( int pDuracion )
+	{
+		duracion = pDuracion;
+	}
+	
+	public void cambiarGenero( String pGenero )
+	{
+		genero = pGenero;
+	}
 }
